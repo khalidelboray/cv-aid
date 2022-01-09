a7ca81d6cac618fbb16043c34401ceed444e0c98
Khalid Mohamed Elborai <accnew820@gmail.com>
2022-01-10 00:18:51 +0200

    More docs needed

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='cv-aid',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    name="cv-aid",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=[
-        'deepstack_sdk==0.2.1',
-        'filetype==1.0.9',
-        'numpy==1.19.5',
-        'opencv_python_inference_engine==2021.10.10',
-        'setuptools==45.2.0',
+        "deepstack_sdk==0.2.1",
+        "filetype==1.0.9",
+        "numpy==1.19.5",
+        "opencv_python_inference_engine==2021.10.10",
+        "setuptools==45.2.0",
     ],
 )
